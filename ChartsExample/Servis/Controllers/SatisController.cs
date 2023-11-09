62dd998dff47123b2695afbefe2a4d3455fc515c
Atakan Demir <101272960+Atakan-Demir@users.noreply.github.com>
2023-11-09 11:43:07 +0300

    Atakan Demir

---
+++ ChartsExample/Servis/Controllers/SatisController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Servis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SatisController : ControllerBase
+    {
+    }
+}
